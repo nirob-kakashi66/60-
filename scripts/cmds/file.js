3495014d11c12f10e417159161f315c7a32bcbec
nirob-kakashi66 <lal1100mia@gmail.com>
2025-07-19 14:24:27 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["61572491867501",];
+		const permission = ["61572240295227",];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage(" You don't have permission to use this command. 🐤", event.threadID, event.messageID);
 		}
