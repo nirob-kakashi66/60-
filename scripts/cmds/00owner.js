989568849e0e73ab8ef255c4a5c64ea0f4915236
nirob-kakashi66 <lal1100mia@gmail.com>
2025-08-12 20:58:55 +0600

    Update 00owner.js

--- scripts/cmds/00owner.js
+++ scripts/cmds/00owner.js
@@ -1,6 +1,6 @@
 module.exports = {
   config: {
-    name: "owner",
+    name: "nirob",
     version: "1.4",
     author: "Tokodori_Frtiz // remodified by cliff",
     countDown: 5,
@@ -22,7 +22,7 @@ module.exports = {
 ╚════════════════════╝
 
 👑 𝗡𝗮𝗺𝗲       : 𝐍𝐢𝐫𝐨𝐛  
-🏡 𝗔𝗱𝗱𝗿𝗲𝘀𝘀   : 𝗗𝗵𝗮𝗸𝗮, 𝗕𝗮𝗻𝗴𝗹𝗮𝗱𝗲𝘀𝗵  
+🏡 𝗔𝗱𝗱𝗿𝗲𝘀𝘀   : 🌚🤌🏻, 𝗕𝗮𝗻𝗴𝗹𝗮𝗱𝗲𝘀𝗵  
 ⚧ 𝗚𝗲𝗻𝗱𝗲𝗿    : 𝗠𝗮𝗹𝗲  
 🎂 𝗔𝗴𝗲        : 𝟭𝟵 𝘆𝗲𝗮𝗿𝘀  
 🦸 𝗡𝗶𝗰𝗸𝗻𝗮𝗺𝗲  : 𝗞𝗮𝗸𝗮𝘀𝗵𝗶  
